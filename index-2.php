5cca52df3377d5017995b93a6d97adb54e164505
Mahak-Patil <MahakPatil@Macs-MacBook-Pro.local>
2015-11-29 20:20:13 -0600

    adding basic css-not too fancy!

---
+++ index-2.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<html>
+<head><title>Hello app</title>
+<meta charset="UTF-8">
+</head>
+<body>
+
+<!-- The data encoding type, enctype, MUST be specified as below -->
+<form enctype="multipart/form-data" action="result.php" method="POST">
+    <!-- MAX_FILE_SIZE must precede the file input field -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+    <!-- Name of input element determines name in $_FILES array -->
+Username: <input type="text" name="firstname"><br />
+Send this file: <input name="userfile" type="file" accept="image/png,image/jpeg"/><br />
+Email of user: <input type="email" name="useremail"><br />
+Phone number (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+<input type="submit" value="Send File" />
+</form>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,20 +1,59 @@
-<?php session_start(); ?>
+<?php 
+session_start(); 
+?>
 <html>
 <head><title>Hello app</title>
 <meta charset="UTF-8">
 </head>
 <body>
+<div align="right">
+<ul>
+<li><a href='gallery.php'/>Images</a></li>
+<li><a href='internal_working.php'/>Backup</a></li>
+</ul>
 
+</div>
+<?php
+if((isset($_SESSION['internals']))&&($_SESSION['internals'])){
+echo "To view gallery, click on Images Images ";
+}
+else
+{
+echo (isset($_SESSION['internals']));
+?>
+
+<div align="center">
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="result.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-Username: <input type="text" name="firstname"><br />
-Send this file: <input name="userfile" type="file" accept="image/png,image/jpeg"/><br />
-Email of user: <input type="email" name="useremail"><br />
-Phone number (1-XXX-XXX-XXXX): <input type="phone" name="phone">
-<input type="submit" value="Send File" />
+<table>
+<tr>
+<td bgcolor="#7FFFD4"> Username: </td>
+<td><input type="text" name="firstname"></td>
+</tr>
+ <tr> 
+<td bgcolor="#7FFFD4">Send this file: </td>
+<td><input name="userfile" type="file" accept="image/png,image/jpeg"/></td>
+</tr>   
+<tr>
+<td bgcolor="#7FFFD4">Email of user: </td>
+<td><input type="email" name="useremail"></td>
+</tr>
+<tr>
+<td bgcolor="#7FFFD4">Phone number (1-XXX-XXX-XXXX): </td>
+<td><input type="phone" name="phone"></td>
+</tr>
+<tr>
+<td colspan="2"><input type="submit" value="Send File" />
+</tr>
+</table>
 </form>
+
+</div>
+<?php
+}
+?>
 </body>
 </html>
