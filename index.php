dcf13ddbb48adbf825ac4d3cf29a343900c924c8
Mahak-Patil <MahakPatil@Macs-MacBook-Pro.local>
2015-11-29 20:23:36 -0600

    correcting css templates

--- index.php
+++ index.php
@@ -30,23 +30,23 @@ echo (isset($_SESSION['internals']));
     <!-- Name of input element determines name in $_FILES array -->
 <table>
 <tr>
-<td bgcolor="#7FFFD4"> Username: </td>
+<td bgcolor="#FA58F4">Username: </td>
 <td><input type="text" name="firstname"></td>
 </tr>
  <tr> 
-<td bgcolor="#7FFFD4">Send this file: </td>
+<td bgcolor="#FA58F4">Send this file: </td>
 <td><input name="userfile" type="file" accept="image/png,image/jpeg"/></td>
 </tr>   
 <tr>
-<td bgcolor="#7FFFD4">Email of user: </td>
+<td bgcolor="#FA58F4">Email of user: </td>
 <td><input type="email" name="useremail"></td>
 </tr>
 <tr>
-<td bgcolor="#7FFFD4">Phone number (1-XXX-XXX-XXXX): </td>
+<td bgcolor="#FA58F4">Phone number (1-XXX-XXX-XXXX): </td>
 <td><input type="phone" name="phone"></td>
 </tr>
 <tr>
-<td colspan="2"><input type="submit" value="Send File" />
+<td colspan="3"><input type="submit" value="Send File" />
 </tr>
 </table>
 </form>
